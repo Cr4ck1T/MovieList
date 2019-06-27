4d64318e75c0b09a9967ac222f6d6b99a1089945
Toni <toni_anders@gmx.de>
2019-06-27 19:13:03 +0200

    add fragment

--- app/src/main/java/de/theothers/movielist/ui/MainFragment.kt
+++ app/src/main/java/de/theothers/movielist/ui/MainFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import de.theothers.movielist.ui.viewModel.MainViewModel
 import de.theothers.movielist.R
 
 class MainFragment : Fragment() {
